e3b1a18d49c065a1ef8eaa32e3087f4782c55744
Mikhail Vetyugov <mikhail.vetyugov@valantmed.com>
2018-10-18 15:00:56 +0300

    update sagas

--- src/sagas.js
+++ src/sagas.js
@@ -1,29 +1,19 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import * as utils from './utils';
 import * as gameActions from 'actions/game-actions';
 
 function* initializeApp() {
-  yield takeLatest(gameActions.INITIALIZE_APP, loadAll);
-}
-
-function* loadVariant() {
-  yield takeLatest(gameActions.LOAD_VARIANT, loadNewVariant);
-}
-
-function* loadAll() {
   yield call(utils.fetchVariantsIds);
   yield put(gameActions.loadVariant());
 }
 
-function* loadNewVariant() {
+function* loadVariant() {
   const id = utils.getFreeId();
   const variant = yield call(utils.fetchVariant, id);
   yield put(gameActions.setVariant(variant));
 }
 
 export default function* rootSaga() {
-  yield all([
-    initializeApp(),
-    loadVariant()
-  ]);
+  yield takeLatest(gameActions.INITIALIZE_APP, initializeApp);
+  yield takeLatest(gameActions.LOAD_VARIANT, loadVariant);
 }
